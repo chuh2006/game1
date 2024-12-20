8f58e78540111bbf49a5aeec014f03ea2d0b112e
初航 <chuh2006@outlook.com>
2024-12-20 19:59:24 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,10 +1,30 @@
 #include "game.cpp"
 
+void menu(){
+    std::cout << "\n-----------------------" << std::endl;
+    std::cout << "| 1. play" << std::endl;
+    std::cout << "| 2. exit" << std::endl;
+    std::cout << "-----------------------\n" << std::endl;
+}
+
 int main(){
-    std::cout << "| input seed:";
-    int seed;
-    std::cin >> seed;
-    Game game(seed);
-    game.processGame();
+    while(true){
+        std::cout << "| input seed:";
+        int seed;
+        std::cin >> seed;
+        Game *game = new Game(seed);
+        game->processGame();
+        delete game;
+        menu();
+        int in;
+        std::cin >> in;
+        switch(in){
+            case 1:
+                continue;
+            case 2:
+                break;
+        }
+        break;
+    }
     return 0;
 }
