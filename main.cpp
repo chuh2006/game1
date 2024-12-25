a9278116f5ec42832e0e4f71743dd306978a9c5c
初航 <chuh2006@outlook.com>
2024-12-25 13:02:44 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,16 +2,24 @@
 
 void menu(){
     std::cout << "\n-----------------------" << std::endl;
-    std::cout << "| 1. play" << std::endl;
-    std::cout << "| 2. exit" << std::endl;
+    std::cout << "| 1.play" << std::endl;
+    std::cout << "| 2.exit" << std::endl;
     std::cout << "-----------------------\n" << std::endl;
 }
 
 int main(){
     while(true){
-        std::cout << "| input seed:";
+        std::cout << "| input seed: ";
         int seed;
         std::cin >> seed;
+        if (seed < 1) {
+            std::cout << "| seed must be greater than 0" << std::endl;
+            continue;
+        }
+        if(seed > 1000){
+            std::cout << "| seed must be less than 1000" << std::endl;
+            continue;
+        }
         Game *game = new Game(seed);
         game->processGame();
         delete game;
